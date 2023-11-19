2bffbc4cc911da4dffe492b3ee7c4ee6af451940
giangdao0211 <148789271+giangdao0211@users.noreply.github.com>
2023-11-19 21:06:05 +0700

    dao

--- QuanLyDiem/QuanLy/obj/Debug/net7.0-windows/App.g.i.cs
+++ QuanLyDiem/QuanLy/obj/Debug/net7.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace QuanLy {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.5.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.8.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace QuanLy {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.5.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.8.0")]
         public static void Main() {
             QuanLy.App app = new QuanLy.App();
             app.InitializeComponent();
