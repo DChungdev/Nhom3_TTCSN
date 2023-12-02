5a27a4ea450132d6597bbfb1f37a940e4aa70495
giangdao0211 <148789271+giangdao0211@users.noreply.github.com>
2023-12-02 14:10:11 +0700

    dao day code

---
+++ QuanLyDiem/QuanLy/QuanLySinhVien.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace QuanLy
+{
+    /// <summary>
+    /// Interaction logic for QuanLySinhVien.xaml
+    /// </summary>
+    public partial class QuanLySinhVien : Window
+    {
+        public QuanLySinhVien()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ThemSinhVien f = new ThemSinhVien();
+            f.Show();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            SuaSinhVien f = new SuaSinhVien();
+            f.Show();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Bạn có chắc chắn muốn xóa dữ liệu sinh viên này không?", "Agreement!", MessageBoxButton.YesNo, MessageBoxImage.Question);
+        }
+    }
+}
--- QuanLyDiem/QuanLy/SuaSinhVien.xaml.cs
+++ QuanLyDiem/QuanLy/SuaSinhVien.xaml.cs
@@ -15,11 +15,11 @@ using System.Windows.Shapes;
 namespace QuanLy
 {
     /// <summary>
-    /// Interaction logic for Window1.xaml
+    /// Interaction logic for SuaSinhVien.xaml
     /// </summary>
-    public partial class Window1 : Window
+    public partial class SuaSinhVien : Window
     {
-        public Window1()
+        public SuaSinhVien()
         {
             InitializeComponent();
         }
